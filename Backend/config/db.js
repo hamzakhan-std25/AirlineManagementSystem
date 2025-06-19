86451463bd52ad302b31d66dd8d2ba9dea08d88e
Hamza khan <hamzakhan.std25@gmail.com>
2025-06-19 18:45:03 +0500

    Remove

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'sql@hum229953409',
+    password: '',
     database: 'AirlineDB'
 });
 module.exports = pool.promise();
